d45b0cab8f3d511d79ba002aecbd2167d386ce60
biondo00 <vlcfnc00h04h579y@studenti.unical.it>
2021-10-20 10:31:57 +0200

    First Commit

---
+++ src/main/java/it/unical/demacs/IngSwGIT_15_10_2021/App.java
@@ -0,0 +1,19 @@
+package it.unical.demacs.IngSwGIT_15_10_2021;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DateTime joda = new DateTime();
+        System.out.println(joda.toString());
+        System.out.println(StringUtils.upperCase("ciaone"));
+    }
+}
